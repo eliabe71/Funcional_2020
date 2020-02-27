6b27ca62f6e278b5fd7b78ba66fbfc4d914e1df7
Eliabe <eliabesantos1106@gmail.com>
2020-02-26 23:38:53 -0300

    n pra frente

---
+++ final.hs
@@ -0,0 +1,6 @@
+final n xs = drop n xs  
+main = do
+    inputdata <- getContents
+    let entrada = lines inputdata
+    let n       = read( entrada !! 0 )
+    print $ final n $ map (read :: String->Int) (tail entrada)
