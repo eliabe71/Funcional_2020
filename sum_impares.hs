ed6e3de06b0c8b02750d17bf23776db2513ce256
Aluno <eliabesantos1106@gmail.com>
2020-02-28 16:47:37 -0300

    modified

--- sum_impares.hs
+++ sum_impares.hs
@@ -1,4 +1,4 @@
-somaImpares xs = sum [ x <- xs, odd x] 
+somaImpares xs = sum [ 1 | _<- xs, odd x] 
 main = do 
 	inputdata <- getContents
     putStrLn $ show $ somaImpares $ map (read :: String -> Int) $ lines inputdata
